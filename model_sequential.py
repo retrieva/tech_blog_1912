8c3e999b517541337587c50494d55089f64c42bb
dwarfer7634 <wild.woodpecker4203@gmail.com>
2019-12-18 08:44:04 +0000

    Added Comments

--- model_sequential.py
+++ model_sequential.py
@@ -11,69 +11,94 @@ import tensorflow_datasets as tfds
 
 from conll_dataset import ConllLoader
 
-EMBEDDING_DIM = 300
-MAX_LENGTH = 32
+BATCH_SIZE = 5 # 訓練時のバッチサイズ
+EPOCHS = 5 # 訓練時のエポック数
+EMBEDDING_DIM = 300 # 学習済み単語分散表現の次元数
+MAX_LENGTH = 32 # 学習データの最大系列長より大きい必要がある
 
-glove = np.concatenate([np.zeros(EMBEDDING_DIM)[np.newaxis], np.load('glove-wiki-300-connl.npy'), 
-        np.zeros(EMBEDDING_DIM)[np.newaxis]], axis=0)
-
-
+# CoNLL 2003 データセットから(stringのlist, stringのlist)の形式で、各種データを読み込みます。
 X_train, y_train = ConllLoader('/mnt/NER/corpus/CoNLL-2003/eng.train').get_list()
 X_dev, y_dev = ConllLoader('/mnt/NER/corpus/CoNLL-2003/eng.testa').get_list()
 X_test_original, y_test = ConllLoader('/mnt/NER/corpus/CoNLL-2003/eng.testb').get_list()
 
-with open('glove-wiki-300-connl.vocab') as f:
+# 以下で学習済み分散表現の読み込みを行います。
+# これによって、訓練データ以外の大規模コーパスから得た単語の意味情報を考慮できます。
+# 0番目はパディング、 (語彙数+1)番目は未知語に割り当てられます。
+glove = np.concatenate([np.zeros(EMBEDDING_DIM)[np.newaxis], np.load('../glove-wiki-300-connl.npy'), 
+        np.zeros(EMBEDDING_DIM)[np.newaxis]], axis=0)
+
+# 学習済み単語分散表現の語彙リストを読み込みます。
+with open('../glove-wiki-300-connl.vocab') as f:
     vocab_list = f.readlines()
 
+# スペース区切り単語列を単語インデックス列に変換するエンコーダを定義。
+word_token_encoder = tfds.features.text.TokenTextEncoder(vocab_list)
 
-def preprocess_data(data, encoder):
-    data = [encoder.encode(sequence) for sequence in data]
-    data = pad_sequences(data, maxlen=MAX_LENGTH, dtype='int32', padding='post', value=0)
-    return data
+# 全ラベルリスト　と ラベル数　の取得
+label_list = list(set([label for label_in_sentence in y_train for label in label_in_sentence.split()]))
+n_labels = len(label_list)
 
-token_encoder = tfds.features.text.TokenTextEncoder(vocab_list)
+# スペース区切りラベル列を単語インデックス列に変換するエンコーダを定義。
+label_token_encoder = tfds.features.text.TokenTextEncoder(label_list)
 
-X_train = preprocess_data(X_train, token_encoder)
-X_dev = preprocess_data(X_dev, token_encoder)
-X_test = preprocess_data(X_test_original, token_encoder)
-
-tag_list = list(set([tag for tag_in_sentence in y_train for tag in tag_in_sentence.split()]))
-n_tags = len(tag_list)
-label_encoder = tfds.features.text.TokenTextEncoder(tag_list)
-
-y_train = preprocess_data(y_train, label_encoder)
-y_dev = preprocess_data(y_dev, label_encoder)
-y_test = preprocess_data(y_test, label_encoder)
+def encode_and_pad_data(sequence_list, encoder):
+    # strのlistを単語インデックスのlistのlistに変換した後、
+    # MAX_LENGTHに満たない文に対して、0でパディングを行う。
+    enceded_sequences = [encoder.encode(sequence) for sequence in sequence_list]
+    encoded_and_padded_sequences = pad_sequences(enceded_sequences, maxlen=MAX_LENGTH, dtype='int32', padding='post', value=0)
+    return encoded_and_padded_sequences
 
 
+# 以下でエンコードとパディングを行います。
+X_train = encode_and_pad_data(X_train, word_token_encoder)
+X_dev = encode_and_pad_data(X_dev, word_token_encoder)
+X_test = encode_and_pad_data(X_test_original, word_token_encoder)
+y_train = encode_and_pad_data(y_train, label_token_encoder)
+y_dev = encode_and_pad_data(y_dev, label_token_encoder)
+y_test = encode_and_pad_data(y_test, label_token_encoder)
 
+
+# 以下でモデルの定義
+# Sequential　APIを使ってモデルの定義
 model = tf.keras.Sequential()
-model.add(Embedding(input_dim=token_encoder.vocab_size, output_dim=EMBEDDING_DIM, 
+# Embedding レイヤーで、 単語インデックスをそれに対応する単語分散表現に置き換えます。
+model.add(Embedding(input_dim=word_token_encoder.vocab_size, output_dim=EMBEDDING_DIM, 
                     embeddings_initializer=tf.keras.initializers.Constant(glove), 
-                    input_shape=(MAX_LENGTH, ), ))#mask_zero=True))
+                    input_shape=(MAX_LENGTH, )))#　mask_zero=True　は上手くいかない
+# 出力次元数200のBiLSTMレイヤーを追加します。
+# LSTMクラスのイニシャライザにreturn_sequences=Trueを渡すことで、
+# 全ての単語に対する予測値を取り出せます。
+# return_sequences=Falseだと1文に対して1つの予測値のみの出力となります。
 model.add(Bidirectional(LSTM(200, return_sequences=True), merge_mode='ave'))
-model.add(Dense(n_tags, activation='softmax'))
-model.add(CRF(n_tags, name='crf_layer'))
+# 出力次元数がタグの種類数の全結合層を追加します。
+# また、活性化関数をsoftmaxにします。
+# これは、CRFレイヤーが各ラベルに対する予測の確信度を要求するためです。
+model.add(Dense(n_labels, activation='softmax'))
+# 最後にCRFレイヤーを追加します。
+model.add(CRF(n_labels, name='crf_layer'))
 
-
+# モデルの初期化を行います。ここで、誤差関数にConditionalRandomFieldLossを指定します。
 model.compile(optimizer='adam', loss=ConditionalRandomFieldLoss())
+# TensorBoard用のログの保存場所を定義します。
 log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-model.fit(X_train, y_train, validation_data=(X_dev, y_dev), epochs=5,  
-    batch_size=5, 
-    callbacks=[tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'), 
-            tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)])
-model.evaluate(X_test, y_test)
+# ここで、学習データを渡すことで、学習を行います。
+model.fit(X_train, y_train, validation_data=(X_dev, y_dev), epochs=EPOCHS,  
+    batch_size=BATCH_SIZE, 
+    callbacks=[tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)])
+# モデルの保存
 model.save('saved_model/model_sequential.h5')
 
-
+# 予測ラベルの取得
 y_pred = model.predict([X_test][:5])
 
+# 予測結果の表示
 for i in range(5):
     print('元の文:', X_test_original[i])    
-    print('正解ラベル:', label_encoder.decode(y_test[i]))    
-    print('予測ラベル:', label_encoder.decode(y_pred[i]))  
+    print('正解ラベル:', label_token_encoder.decode(y_test[i]))    
+    print('予測ラベル:', label_token_encoder.decode(y_pred[i]))  
     print()  
     with open('ner_results.txt', 'a') as f:
         f.write('元の文:'+X_test_original[i]+'\n')
-        f.write('正解ラベル:'+label_encoder.decode(y_test[i])+'\n')
-        f.write('予測ラベル:'+label_encoder.decode(y_pred[i])+'\n\n')
+        f.write('正解ラベル:'+label_token_encoder.decode(y_test[i])+'\n')
+        f.write('予測ラベル:'+label_token_encoder.decode(y_pred[i])+'\n\n')
+
--- model_subclass.py
+++ model_subclass.py
@@ -11,39 +11,53 @@ import tensorflow_datasets as tfds
 
 from conll_dataset import ConllLoader
 
-EMBEDDING_DIM = 300
-MAX_LENGTH = 32
+BATCH_SIZE = 5 # 訓練時のバッチサイズ
+EPOCHS = 5 # 訓練時のエポック数
+EMBEDDING_DIM = 300 # 学習済み単語分散表現の次元数
+MAX_LENGTH = 32 # 学習データの最大系列長より大きい必要がある
 
-
-glove = np.concatenate([np.zeros(EMBEDDING_DIM)[np.newaxis], np.load('glove-wiki-300-connl.npy'), np.zeros(EMBEDDING_DIM)[np.newaxis]], axis=0)
-
-
+# CoNLL 2003 データセットから(stringのlist, stringのlist)の形式で、各種データを読み込みます。
 X_train, y_train = ConllLoader('/mnt/NER/corpus/CoNLL-2003/eng.train').get_list()
 X_dev, y_dev = ConllLoader('/mnt/NER/corpus/CoNLL-2003/eng.testa').get_list()
-X_test, y_test = ConllLoader('/mnt/NER/corpus/CoNLL-2003/eng.testb').get_list()
+X_test_original, y_test = ConllLoader('/mnt/NER/corpus/CoNLL-2003/eng.testb').get_list()
 
-with open('glove-wiki-300-connl.vocab') as f:
+# 以下で学習済み分散表現の読み込みを行います。
+# これによって、訓練データ以外の大規模コーパスから得た単語の意味情報を考慮できます。
+# 0番目はパディング、 (語彙数+1)番目は未知語に割り当てられます。
+glove = np.concatenate([np.zeros(EMBEDDING_DIM)[np.newaxis], np.load('../glove-wiki-300-connl.npy'), 
+        np.zeros(EMBEDDING_DIM)[np.newaxis]], axis=0)
+
+# 学習済み単語分散表現の語彙リストを読み込みます。
+with open('../glove-wiki-300-connl.vocab') as f:
     vocab_list = f.readlines()
 
-def preprocess_data(data, encoder):
-    data = [encoder.encode(sequence) for sequence in data]
-    data = pad_sequences(data, maxlen=MAX_LENGTH, dtype='int32', padding='post', value=0)
-    return data
+# スペース区切り単語列を単語インデックス列に変換するエンコーダを定義。
+word_token_encoder = tfds.features.text.TokenTextEncoder(vocab_list)
 
-token_encoder = tfds.features.text.TokenTextEncoder(vocab_list)
+# 全ラベルリスト　と ラベル数　の取得
+label_list = list(set([label for label_in_sentence in y_train for label in label_in_sentence.split()]))
+n_labels = len(label_list)
 
-X_train = preprocess_data(X_train, token_encoder)
-X_dev = preprocess_data(X_dev, token_encoder)
-X_test = preprocess_data(X_test, token_encoder)
+# スペース区切りラベル列を単語インデックス列に変換するエンコーダを定義。
+label_token_encoder = tfds.features.text.TokenTextEncoder(label_list)
 
-tag_list = list(set([tag for tag_in_sentence in y_train for tag in tag_in_sentence.split()]))
-n_tags = len(tag_list)
-label_encoder = tfds.features.text.TokenTextEncoder(tag_list)
+def encode_and_pad_data(sequence_list, encoder):
+    # strのlistを単語インデックスのlistのlistに変換した後、
+    # MAX_LENGTHに満たない文に対して、0でパディングを行う。
+    enceded_sequences = [encoder.encode(sequence) for sequence in sequence_list]
+    encoded_and_padded_sequences = pad_sequences(enceded_sequences, maxlen=MAX_LENGTH, dtype='int32', padding='post', value=0)
+    return encoded_and_padded_sequences
 
-y_train = preprocess_data(y_train, label_encoder)
-y_dev = preprocess_data(y_dev, label_encoder)
-y_test = preprocess_data(y_test, label_encoder)
 
+# 以下でエンコードとパディングを行います。
+X_train = encode_and_pad_data(X_train, word_token_encoder)
+X_dev = encode_and_pad_data(X_dev, word_token_encoder)
+X_test = encode_and_pad_data(X_test_original, word_token_encoder)
+y_train = encode_and_pad_data(y_train, label_token_encoder)
+y_dev = encode_and_pad_data(y_dev, label_token_encoder)
+y_test = encode_and_pad_data(y_test, label_token_encoder)
+
+# tf.keras.Model　を継承してモデルを定義する。
 class BilstmCRF(tf.keras.Model):
     def __init__(self, num_tags, token_encoder, label_encoder, embedding_list):
         super().__init__()
@@ -53,9 +67,9 @@ class BilstmCRF(tf.keras.Model):
 
         self.embedding_layer = Embedding(input_dim=token_encoder.vocab_size, output_dim=EMBEDDING_DIM, 
                                          embeddings_initializer=tf.keras.initializers.Constant(embedding_list), mask_zero=True)
-        self.bilstm_layer = Bidirectional(LSTM(400, return_sequences=True), merge_mode='ave')
-        self.dense_layer = Dense(n_tags, activation='softmax')
-        self.crf_layer = CRF(n_tags, name='crf_layer')
+        self.bilstm_layer = Bidirectional(LSTM(200, return_sequences=True), merge_mode='ave')
+        self.dense_layer = Dense(num_tags, activation='softmax')
+        self.crf_layer = CRF(num_tags, name='crf_layer')
 
     @tf.function(autograph=True) # autograph=Falseでグラフモードへの変換を停止
     def call(self, inputs):
@@ -72,13 +86,33 @@ class BilstmCRF(tf.keras.Model):
         return loss
 
 
-
-model = BilstmCRF(n_tags, token_encoder, label_encoder, glove)
+# 以下でモデルの定義
+# 独自クラスによってモデルを作る。
+model = BilstmCRF(n_labels, word_token_encoder, label_token_encoder, glove)
+# モデルの初期化を行います。ここで、誤差関数にBilstmCRFのcrf_lossメソッドを指定します。
 model.compile(optimizer='adam', loss=model.crf_loss, metrics=['accuracy'])
+# TensorBoard用のログの保存場所を定義します。
 log_dir="logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-model.fit(X_train, y_train, validation_data=(X_dev, y_dev), epochs=5,  
-    batch_size=5, 
-    callbacks=[tf.keras.callbacks.EarlyStopping(patience=2, monitor='val_loss'), 
-               tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)])
-model.evaluate(X_test, y_test)
+# ここで、学習データを渡すことで、学習を行います。
+model.fit(X_train, y_train, validation_data=(X_dev, y_dev), epochs=EPOCHS,  
+    batch_size=BATCH_SIZE, 
+    callbacks=[tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)])
+
+# モデルの保存
+model.save('saved_model/model_subclass.h5')
+
+# 予測ラベルの取得
+y_pred = model.predict([X_test][:5])
+
+# 予測結果の表示
+for i in range(5):
+    print('元の文:', X_test_original[i])    
+    print('正解ラベル:', label_token_encoder.decode(y_test[i]))    
+    print('予測ラベル:', label_token_encoder.decode(y_pred[i]))  
+    print()  
+    with open('ner_results.txt', 'a') as f:
+        f.write('元の文:'+X_test_original[i]+'\n')
+        f.write('正解ラベル:'+label_token_encoder.decode(y_test[i])+'\n')
+        f.write('予測ラベル:'+label_token_encoder.decode(y_pred[i])+'\n\n')
+
 
